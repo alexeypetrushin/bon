2b065ff65ed0c345782f4d2e174411ad46f30922
alexeypetrushin <alexey.petrushin@gmail.com>
2019-07-25 00:19:46 +1000

    Upd

--- fs.ts
+++ fs.ts
@@ -1,95 +1,77 @@
 import { something, assert } from './base'
+import * as nodefs from 'fs'
+import * as nodepath from 'path'
+import * as fsextra from 'fs-extra'
+import { promisify } from 'util'
 
-function withCache<T>(fn: () => T): () => T {
-  let cached: T | undefined = undefined
-  return function cacheWrapper() {
-    cached = cached === undefined ? fn() : cached
-    return cached
-  }
-}
-const nodefs = withCache(() => require('fs'))
-const nodepath = withCache(() => require('path'))
-const fsextra = withCache(() => require('fs-extra'))
-export function promisify(fn: Function): something {
-  return function (...args: something[]) {
-    return new Promise((resolve: something, reject: something) => {
-      fn(...[
-        ...args,
-        function promisifyCallback(err: something, result: something) { err ? reject(err) : resolve(result) }
-      ])
-    })
-  }
+export function resolve(...paths: string[]): string { return require('path').resolve(...paths) }
+
+export async function readDirectory(path: string): Promise<string[]> {
+  return (await promisify(nodefs.readdir)(path)).map((name: string) => resolve(path, name))
 }
 
-class Fs {
-  resolve(...paths: string[]): string { return require('path').resolve(...paths) }
+function readFile(path: string): Promise<Buffer>
+function readFile(path: string, options: { encoding: BufferEncoding }): Promise<string>
+function readFile(path: string, options?: something) {
+  return promisify(nodefs.readFile)(path, options) as something
+}
+export { readFile }
 
-  async readDirectory(path: string): Promise<string[]> {
-    return (await promisify(nodefs().readdir)(path)).map((name: string) => this.resolve(path, name))
-  }
+// Creates parent directory automatically
+export async function writeFile(
+  path:     string,
+  data:     something,
+  options?: { encoding?: BufferEncoding, flag?: string }
+): Promise<void> {
+  // Creating parent directory for to if it's not exist.
+  // Could be speed up by handling exception instead of checking for existance.
+  const directory = nodepath.dirname(path)
+  if (!(await exists(directory))) await makeDirectory(directory)
 
-  readFile(path: string): Promise<Buffer>
-  readFile(path: string, options: { encoding: BufferEncoding }): Promise<string>
-  readFile(path: string, options?: something) { return promisify(nodefs().readFile)(path, options) }
+  await promisify(nodefs.writeFile)(path, data, options)
+}
 
-  // Creates parent directory automatically
-  async writeFile(
-    path:     string,
-    data:     something,
-    options?: { encoding?: BufferEncoding, flag?: string }
-  ): Promise<void> {
-    // Creating parent directory for to if it's not exist.
-    // Could be speed up by handling exception instead of checking for existance.
-    const directory = nodepath().dirname(path)
-    if (!(await fs.exists(directory))) await fs.makeDirectory(directory)
+export async function rename(from: string, to: string, options?: { overwrite: boolean }): Promise<void> {
+  // Checking if to file already exist
+  if ((await exists(to)) && !(options && options.overwrite)) throw new Error(`file ${to} already exists`)
 
-    await promisify(nodefs().writeFile)(path, data, options)
-  }
+  // Creating parent directory for to if it's not exist
+  const toParentDirectory = nodepath.dirname(to)
+  if (!(await exists(toParentDirectory))) await makeDirectory(toParentDirectory)
 
-  async rename(from: string, to: string, options?: { overwrite: boolean }): Promise<void> {
-    // Checking if to file already exist
-    if ((await fs.exists(to)) && !(options && options.overwrite)) throw new Error(`file ${to} already exists`)
+  await promisify(nodefs.rename)(from, to)
+}
 
-    // Creating parent directory for to if it's not exist
-    const toParentDirectory = nodepath().dirname(to)
-    if (!(await fs.exists(toParentDirectory))) await fs.makeDirectory(toParentDirectory)
+// Creates parent directory automatically
+export async function makeDirectory(path: string): Promise<void> {
+  // Could be speed up by handling exception instead of checking for existance.
+  if ((await exists(path))) return
+  await (promisify(fsextra.ensureDir) as something)(path)
+}
 
-    await promisify(nodefs().rename)(from, to)
-  }
-
-  // Creates parent directory automatically
-  async makeDirectory(path: string): Promise<void> {
-    // Could be speed up by handling exception instead of checking for existance.
-    if ((await fs.exists(path))) return
-    await promisify(fsextra().ensureDir)(path)
-  }
-
-  async exists(path: string): Promise<boolean> {
-    try {
-      await promisify(nodefs().stat)(path)
-      return true
-    } catch (e) {
-      return false
-    }
-  }
-
-  async deleteFile(path: string): Promise<void> {
-    // Check for existance is optional, probably could be removed
-    if (!(await this.exists(path))) return
-    await promisify(nodefs().unlink)(path)
-  }
-
-  async deleteDirectory(path: string, options?: { recursive?: boolean }): Promise<void> {
-    // Reqursive flag requiret for safety.
-    if (!(await this.exists(path))) return
-    await promisify(options && options.recursive ? fsextra().remove : nodefs().rmdir)(path)
-  }
-
-  async deleteTmpDirectory(path: string): Promise<void> {
-    // `tmp` in path required for safety so you don't accidentally delete non temp directory.
-    assert(/tmp|temp/i.test(path), `temp directory expected to have tmp or temp term in its path`)
-    this.deleteDirectory(path, { recursive: true })
+export async function exists(path: string): Promise<boolean> {
+  try {
+    await promisify(nodefs.stat)(path)
+    return true
+  } catch (e) {
+    return false
   }
 }
 
-export const fs = new Fs()
+export async function deleteFile(path: string): Promise<void> {
+  // Check for existance is optional, probably could be removed
+  if (!(await exists(path))) return
+  await promisify(nodefs.unlink)(path)
+}
+
+export async function deleteDirectory(path: string, options?: { recursive?: boolean }): Promise<void> {
+  // Reqursive flag requiret for safety.
+  if (!(await exists(path))) return
+  await promisify(options && options.recursive ? fsextra.remove : nodefs.rmdir)(path)
+}
+
+export async function deleteTmpDirectory(path: string): Promise<void> {
+  // `tmp` in path required for safety so you don't accidentally delete non temp directory.
+  assert(/tmp|temp/i.test(path), `temp directory expected to have tmp or temp term in its path`)
+  deleteDirectory(path, { recursive: true })
+}
